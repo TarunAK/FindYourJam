c630e6d2fa18afc10f4e6c47b2b261fc4407464e
Dexeux <cgaboury@live.com>
2017-01-29 04:04:20 -0500

    Finished attending events

--- android_app/FindYourJam/app/src/main/java/com/example/tarunkalikivaya/findyourjam/EventDisplay.java
+++ android_app/FindYourJam/app/src/main/java/com/example/tarunkalikivaya/findyourjam/EventDisplay.java
@@ -3,8 +3,12 @@ package com.example.tarunkalikivaya.findyourjam;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.tarunkalikivaya.findyourjam.eventlistattend.DisplayEventListAttendActivity;
 
 import org.json.JSONObject;
 
@@ -24,7 +28,7 @@ public class EventDisplay extends AppCompatActivity {
     private TextView mDescription;
     private TextView mDate;
     private TextView mLocation;
-
+    private Button mAttend;
 
     @Override
     public void onCreate(Bundle savedInstances){
@@ -35,6 +39,14 @@ public class EventDisplay extends AppCompatActivity {
         mDescription = (TextView)findViewById(R.id.ev_description);
         mDate = (TextView)findViewById(R.id.ev_date);
         mLocation = (TextView)findViewById(R.id.ev_location);
+        mAttend = (Button)findViewById(R.id.attend);
+        mAttend.setVisibility(View.GONE);
+        mAttend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Attend().execute(Constants.getToken(getBaseContext()),getIntent().getStringExtra("id"));
+            }
+        });
 
         new GetEvent().execute(Constants.getToken(this),getIntent().getStringExtra("id"));
     }
@@ -48,7 +60,7 @@ public class EventDisplay extends AppCompatActivity {
             String token = str[0];
             String id = str[1];
             String status = "";
-            String urlEndPoint = Constants.WEB_URL + "/api/v1/auth/get_event" ;
+            String urlEndPoint = Constants.WEB_URL + "/api/v1/events/get_event" ;
             JSONObject obj = new JSONObject();
             try {
                 obj.put("event_id",id);
@@ -64,12 +76,14 @@ public class EventDisplay extends AppCompatActivity {
                 client.setConnectTimeout(10000);
                 client.setRequestMethod("POST");
                 client.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                client.setRequestProperty("Authorization", " Token " + token);
                 client.setDoOutput(true);
                 OutputStream os = client.getOutputStream();
                 os.write(obj.toString().getBytes("UTF-8"));
                 os.flush();
                 os.close();
-                if(client.getResponseCode()!= 200){
+                int t = client.getResponseCode();
+                if(t!= 200){
                     return null;
                 }
 
@@ -81,11 +95,12 @@ public class EventDisplay extends AppCompatActivity {
                 if (!status.equals("success")){
                     return   null;
                 }
-                String[] array = new String[4];
+                String[] array = new String[5];
                 array[0] = response.getString("title");
                 array[1] = response.getString("description");
                 array[2] = response.getString("date");
                 array[3] = response.getString("location");
+                array[4] = response.getString("same");
                 return  array;
             }catch (Exception e){
                 e.printStackTrace();
@@ -107,9 +122,83 @@ public class EventDisplay extends AppCompatActivity {
                 mDescription.setText(output[1]);
                 mDate.setText(output[2]);
                 mLocation.setText(output[3]);
+                if(output[4].equals("false")){
+                    mAttend.setVisibility(View.VISIBLE);
+                }
             }else{
                 Toast.makeText(getApplicationContext(),"Error Loading Event",Toast.LENGTH_SHORT).show();
                 finish();
+            }
+        }
+    }
+
+    /**
+     * Start a login network call
+     */
+    private class Attend extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(String ... str){
+            String token = str[0];
+            String id = str[1];
+            String status = "";
+            String urlEndPoint = Constants.WEB_URL + "/api/v1/events/attend_event" ;
+            JSONObject obj = new JSONObject();
+            try {
+                obj.put("event_id",id);
+            }catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+            //Run the api call
+            HttpURLConnection client = null;
+            try {
+                URL url = new URL(urlEndPoint);
+                client =(HttpURLConnection) url.openConnection();
+                client.setConnectTimeout(10000);
+                client.setRequestMethod("POST");
+                client.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                client.setRequestProperty("Authorization", " Token " + token);
+                client.setDoOutput(true);
+                OutputStream os = client.getOutputStream();
+                os.write(obj.toString().getBytes("UTF-8"));
+                os.flush();
+                os.close();
+                int t = client.getResponseCode();
+                if(t!= 200){
+                    return null;
+                }
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                String text = br.readLine();
+                JSONObject response= new JSONObject(text);
+
+                status = response.getString("status");
+                if (!status.equals("success")){
+                    return   null;
+                }
+                return  true;
+            }catch (Exception e){
+                e.printStackTrace();
+                if(client!=null){
+                    client.disconnect();
+                }
+                return null;
+            }finally {
+                if(client!=null){
+                    client.disconnect();
+                }
+            }
+
+        }
+        @Override
+        protected void onPostExecute(Boolean output){
+            if(output){
+                Toast.makeText(getApplicationContext(),"Attending Event",Toast.LENGTH_SHORT).show();
+                finish();
+
+            }else{
+                Toast.makeText(getApplicationContext(),"Error Attending Event",Toast.LENGTH_SHORT).show();
+                //finish();
             }
         }
     }
